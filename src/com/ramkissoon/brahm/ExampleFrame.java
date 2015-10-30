ab3168f21ce6aad6c4c5fce805323949409baf61
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-30 06:01:50 -0400

    added JFrame Class

---
+++ src/com/ramkissoon/brahm/ExampleFrame.java
@@ -0,0 +1,24 @@
+package com.ramkissoon.brahm;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ExampleFrame extends JFrame {
+	// Private Instance Variables
+	private JPanel _contentPanel;
+	
+
+	// Constructor +++++++++++++++++++++++++++++++++++++++++
+	public ExampleFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		this._contentPanel = new JPanel();
+		this._contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this._contentPanel.setLayout(new BorderLayout(0, 0));
+		setContentPane(this._contentPanel);
+	}
+}
---
+++ src/com/ramkissoon/brahm/Program.java
@@ -0,0 +1,10 @@
+package com.ramkissoon.brahm;
+
+// Driver Class
+public class Program {
+	
+	public static void main(String[] args){
+		ExampleFrame frame = new ExampleFrame();
+		frame.setVisible(true);
+	}	
+}
