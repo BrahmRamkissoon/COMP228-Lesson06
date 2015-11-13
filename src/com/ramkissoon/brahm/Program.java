2f6d23ddf05d4611dd7ead09272da23cdf747af2
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-11-13 10:38:14 -0500

    added custom dialog

--- src/com/ramkissoon/brahm/Program.java
+++ src/com/ramkissoon/brahm/Program.java
@@ -2,7 +2,10 @@ package com.ramkissoon.brahm;
 
 import java.awt.BorderLayout;
 
+import javax.swing.Icon;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -11,7 +14,25 @@ import javax.swing.border.EmptyBorder;
 public class Program {
 	
 	public static void main(String[] args){
-		ExampleFrame myFrame = new ExampleFrame();
-		myFrame.setVisible(true);
+		/*ExampleFrame myFrame = new ExampleFrame();
+		myFrame.setVisible(true);*/
+		
+		String firstName;
+		String lastName;
+		int messageReturn;
+		
+		//firstName = JOptionPane.showInputDialog("Enter Your First Name");
+		//lastName = JOptionPane.showInputDialog("Enter Your Last Name");
+		
+		//String output = "FirstName: " + firstName + " LastName: " + lastName;
+		
+		//JOptionPane.showMessageDialog(null, output, "Profile Info", JOptionPane.PLAIN_MESSAGE);
+		//messageReturn = JOptionPane.showConfirmDialog(null, "Error - Incorrect Input", "Error", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+		//System.out.println(messageReturn);
+		
+		MyDialog myDialog = new MyDialog();
+		myDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		myDialog.setVisible(true);
+		
 	}	
 }
